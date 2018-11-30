f872b49178757ecaf26e2951add4fda4df53b69a
Yu Li <yl4019@columbia.edu>
2018-11-29 23:38:47 -0500

    Extracted Net class to model.py and created colorize.ipynb to load model and colorize image

--- model.py
+++ model.py
@@ -8,169 +8,138 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ########################################################################
-# The output of torchvision datasets are PILImage images of range [0, 1].
-# We transform them to Tensors of normalized range [-1, 1].
-
-# TODO: INITIALIZE DATALOADERS HERE
-# Credit: http://blog.outcome.io/pytorch-quick-start-classifying-an-image/ (for loading ImageNet classes and images)
-"""
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-"""
-
-########################################################################
-# functions to show an image
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-if __name__ == '__main__':
-
-# hardcoded input height/width parameters (as recommended by S. Iizuka et. al. paper)
-
-    h = int(224)
-    w = int(224)
-
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
-
-    # show images
-    imshow(torchvision.utils.make_grid(images))
-    # print labels
-    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-########################################################################
 # Define the Convolution Neural Networks for colorization
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
+class Net(nn.Module):
+    def __init__(self):
+        """
+        Composing the layers for a sequential network
+        Max-pooling is not used as such a pooling layer will distort the image
+        Instead, we use a stride of 2 to improve info density (see: https://blog.floydhub.com/colorizing-b-w-photos-with-neural-networks/)
+        Intiutively, the conv filter "moves" through the image in 2-pixel steps instead of 1
+        
+        The input images are of size 224 * 224.
+        The number of classes for the classification network is 365 (dataset: http://places2.csail.mit.edu/)
+        """
+        super(Net, self).__init__()
+        
+        h = 224
+        num_classes = 365
+        # Low-level features network
+        layers = []
+        layers.append(nn.Conv2d(1, int(h/4), 3, stride=2, padding=1))
+        layers.append(nn.BatchNorm2d(int(h/4)))
+        layers.append(nn.ReLU())
+        layers.append(nn.Conv2d(int(h/4), int(h/2), 3, stride=1, padding=1))
+        layers.append(nn.BatchNorm2d(int(h/2)))
+        layers.append(nn.ReLU())
+        layers.append(nn.Conv2d(int(h/2), int(h/2), 3, stride=2, padding=1))
+        layers.append(nn.BatchNorm2d(int(h/2)))
+        layers.append(nn.ReLU())
+        layers.append(nn.Conv2d(int(h/2), int(h), 3, stride=1, padding=1))
+        layers.append(nn.BatchNorm2d(int(h)))
+        layers.append(nn.ReLU())
+        layers.append(nn.Conv2d(int(h), int(h), 3, stride=2, padding=1))
+        layers.append(nn.BatchNorm2d(int(h)))
+        layers.append(nn.ReLU())
+        layers.append(nn.Conv2d(int(h), int(h*2), 3, stride=1, padding=1))
+        layers.append(nn.BatchNorm2d(int(h*2)))
+        layers.append(nn.ReLU())
+        self.low_features = nn.Sequential(*layers)
 
-            """
-            Composing the layers for a sequential network
-            Max-pooling is not used as such a pooling layer will distort the image
-            Instead, we use a stride of 2 to improve info density (see: https://blog.floydhub.com/colorizing-b-w-photos-with-neural-networks/)
-            Intiutively, the conv filter "moves" through the image in 2-pixel steps instead of 1
-            
-            Also, unlike Keras, there is no padding=same option for the filters and colorization requires us to keep the image size the same
-            Therefore, we use the formula padding = (kernel_size - stride)//2
-            See discussion on https://github.com/pytorch/pytorch/issues/3867
-            """
-            #h = 224
-            layers = []
+        # Global features network
+        global_conv_layers = []
+        global_conv_layers.append(nn.Conv2d(int(h*2), int(h*2), 3, stride=2, padding=1))
+        global_conv_layers.append(nn.BatchNorm2d(int(h*2)))
+        global_conv_layers.append(nn.ReLU())
+        global_conv_layers.append(nn.Conv2d(int(h*2), int(h*2), 3, stride=1, padding=1))
+        global_conv_layers.append(nn.BatchNorm2d(int(h*2)))
+        global_conv_layers.append(nn.ReLU())
+        global_conv_layers.append(nn.Conv2d(int(h*2), int(h*2), 3, stride=2, padding=1))
+        global_conv_layers.append(nn.BatchNorm2d(int(h*2)))
+        global_conv_layers.append(nn.ReLU())
+        global_conv_layers.append(nn.Conv2d(int(h*2), int(h*2), 3, stride=1, padding=1))
+        global_conv_layers.append(nn.BatchNorm2d(int(h*2)))
+        global_conv_layers.append(nn.ReLU())
+        self.global_conv_features = nn.Sequential(*global_conv_layers)
+        
+        global_fc_layers = []
+        global_fc_layers.append(nn.Linear(int(h/32)*int(h/32)*int(h*2), int(h*4)))
+        global_fc_layers.append(nn.ReLU())
+        global_fc_layers.append(nn.Linear(int(h*4), int(h*2)))
+        global_fc_layers.append(nn.ReLU())
+        self.global_fc_features = nn.Sequential(*global_fc_layers)
+        
+        final_fc_layers = []
+        final_fc_layers.append(nn.Linear(int(h*2), int(h)))
+        final_fc_layers.append(nn.ReLU())
+        self.final_fc_features = nn.Sequential(*final_fc_layers)
+        
+        # Classification network
+        class_layers = []
+        class_layers.append(nn.Linear(int(h*2), int(h)))
+        class_layers.append(nn.ReLU())
+        class_layers.append(nn.Linear(int(h), num_classes))
+        self.class_features = nn.Sequential(*class_layers)
+        
+        # Mid-Level features network
+        mid_features_layers = []
+        mid_features_layers.append(nn.Conv2d(int(h*2), int(h*2), 3, stride=1, padding=1))
+        mid_features_layers.append(nn.BatchNorm2d(int(h*2)))
+        mid_features_layers.append(nn.ReLU())
+        mid_features_layers.append(nn.Conv2d(int(h*2), int(h), 3, stride=1, padding=1))
+        mid_features_layers.append(nn.BatchNorm2d(int(h)))
+        mid_features_layers.append(nn.ReLU())
+        self.mid_features = nn.Sequential(*mid_features_layers)
 
-            layers.append(nn.Conv2d(int(h/8), int(h/4), 3, stride=2, padding=(3 - 2)//2))
-            layers.append(nn.Conv2d(int(h/4), int(h/2), 3, stride=1, padding=(3 - 1)//2))
+        # Colorization network
+        fusion_layers = []
+        fusion_layers.append(nn.Linear(int(h*2), int(h)))
+        fusion_layers.append(nn.ReLU())
+        self.fusion_features = nn.Sequential(*fusion_layers)
+        
+        color_layers1 = []
+        color_layers1.append(nn.Conv2d(int(h), int(h/2), 3, stride=1, padding=1))
+        color_layers1.append(nn.BatchNorm2d(int(h/2)))
+        color_layers1.append(nn.ReLU())
+        self.color1 = nn.Sequential(*color_layers1)
 
-            layers.append(nn.Conv2d(int(h/4), int(h/2), 3, stride=2, padding=(3 - 2)//2))
-            layers.append(nn.Conv2d(int(h/2), int(h), 3, stride=1, padding=(3 - 1)//2))
+        color_layers2 = []
+        color_layers2.append(nn.Conv2d(int(h/2), int(h/4), 3, stride=1, padding=1))
+        color_layers2.append(nn.BatchNorm2d(int(h/4)))
+        color_layers2.append(nn.ReLU())
+        color_layers2.append(nn.Conv2d(int(h/4), int(h/4), 3, stride=1, padding=1))
+        color_layers2.append(nn.BatchNorm2d(int(h/4)))
+        color_layers2.append(nn.ReLU())
+        self.color2 = nn.Sequential(*color_layers2)
 
-            layers.append(nn.Conv2d(int(h/2), int(h), 3, stride=2, padding=(3 - 2)//2))
-            layers.append(nn.Conv2d(int(h), int(h*2), 3, stride=1, padding=(3 - 1) // 2))
-
-            #low-level features network
-            self.low_features = nn.Sequential(*layers)
-
-            global_layers = []
-            global_layers.append(nn.Conv2d(int(h*2), int(h*2), 3, stride=2, padding=(3 - 2)//2))
-            global_layers.append(nn.Conv2d(int(h*2), int(h*2), 3, stride=1, padding=(3 - 1) // 2))
-            global_layers.append(nn.Conv2d(int(h * 2), int(h * 2), 3, stride=2, padding=(3 - 2) // 2))
-            global_layers.append(nn.Conv2d(int(h * 2), int(h * 2), 3, stride=1, padding=(3 - 1) // 2))
-            global_layers.append(nn.Linear(int(h*2), int(h*4)))
-            global_layers.append(nn.Linear(int(h*4), int(h*2)))
-            global_layers.append(nn.Linear(int(h*2), int(h)))
-
-            self.global_features = nn.Sequential(*global_layers)
-
-            mid_features_layers = []
-            mid_features_layers.append(nn.Conv2d(int(h * 2), int(h * 2), 3, stride=1, padding=(3 - 1) // 2))
-            mid_features_layers.append(nn.Conv2d(int(h), int(h * 2), 3, stride=1, padding=(3 - 1) // 2))
-
-            self.mid_features = nn.Sequential(*mid_features_layers)
-
-            color_layers1 = []
-            color_layers1.append(nn.Conv2d(int(h/2), int(h), 3, stride=1, padding=(3 - 1) // 2))
-            self.color1 = nn.Sequential(*color_layers1)
-
-            color_layers2 = []
-            color_layers2.append(nn.Conv2d(int(h/4), int(h/2), 3, stride=1, padding=(3 - 1) // 2))
-            color_layers2.append(nn.Conv2d(int(h / 4), int(h/4), 3, stride=1, padding=(3 - 1) // 2))
-            self.color2 = nn.Sequential(*color_layers2)
-
-            color_layers3 = []
-            color_layers3.append(nn.Conv2d(int(h/8), int(h/4), 3, stride=1, padding=(3 - 1) // 2))
-            color_layers3.append(nn.Conv2d(2, int(h/8), 3, stride=1, padding=(3 - 1) // 2))
-            self.color3 = nn.Sequential(*color_layers3)
-
-        def forward(self, img):
-            img = self.low_features(img)
-            global_img = img
-            global_img = self.global_features(global_img)
-            img = self.mid_features(img)
-            combined = torch.cat((img.view(img.size(0), -1),global_img.view(global_img.size(0), -1)), dim=1)
-            combined = self.color1(combined)
-            combined = F.upsample(combined, scale_factor=2, mode='nearest')
-            combined = self.color2(combined)
-            combined = F.upsample(combined, scale_factor=2, mode='nearest')
-            combined = self.color3(combined)
-
-            return combined, global_img
-
-    net = Net()
-
-# TODO: DEFINE LOSS FUNCTION HERE
-########################################################################
-# Define a Loss function and optimizer
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # Let's use a Classification Cross-Entropy loss and SGD with momentum.
-
-    import torch.optim as optim
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
-#TRAIN THE NETWORK
-########################################################################
-# Training the network
-# ^^^^^^^^^^^^^^^^^^^^
-    for epoch in range(10):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-
-    print('Finished Training')
+        color_layers3 = []
+        color_layers3.append(nn.Conv2d(int(h/4), int(h/8), 3, stride=1, padding=1))
+        color_layers2.append(nn.BatchNorm2d(int(h/8)))
+        color_layers3.append(nn.ReLU())
+        color_layers3.append(nn.Conv2d(int(h/8), 2, 3, stride=1, padding=1))
+        color_layers3.append(nn.Sigmoid())
+        self.color3 = nn.Sequential(*color_layers3)
+        
+    def forward(self, img):
+        img = img.permute((0,3,1,2))
+        img = self.low_features(img)
+        global_img = self.global_conv_features(img)
+        global_img = global_img.view(global_img.size(0), -1)
+        global_img = self.global_fc_features(global_img)
+        label_preds = self.class_features(global_img)
+        global_img = self.final_fc_features(global_img)
+        img = self.mid_features(img)
+        fusion = torch.cat((img.permute(2, 3, 0, 1), global_img.repeat(img.size(2), img.size(3), 1, 1)), dim=-1)
+        fusion = self.fusion_features(fusion)
+        fusion = fusion.permute(2, 3, 0, 1)
+        combined = self.color1(fusion)
+        combined = F.interpolate(combined, scale_factor=2, mode='nearest')
+        combined = self.color2(combined)
+        combined = F.interpolate(combined, scale_factor=2, mode='nearest')
+        combined = self.color3(combined)
+        color_preds = F.interpolate(combined, scale_factor=2, mode='nearest')
+        color_preds = color_preds.permute(0, 2, 3, 1)
+        return color_preds, label_preds
+    
